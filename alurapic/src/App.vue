6a921cc1b18ba99192b7cfabb3c7d0fef57b8447
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-11-21 18:23:24 -0300

    Estilizando projeto

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,60 +1,73 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png" alt="Logo" title="Logo">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="container">
+	<h1>{{ title }}</h1>
+	<div class="content" >
+		<div v-for="foto of fotos" class="images-content">
+			<h2>{{ foto.titulo }}</h2>
+			<img :src='foto.url' :title='foto.titulo' :alt='foto.titulo' class='images'>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Projeto Alura Pic'
-    }
-  }
+	data() {
+		return {
+			title: 'Projeto Alura Pic',
+			fotos: []
+		}
+	},
+	created() {
+		this.$http.get('http://localhost:3000/v1/fotos')
+			.then(response => response.json())
+			.then(fotos => this.fotos = fotos)
+			.catch(err => console.log(err))
+		console.log(this.fotos)
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	margin: 0;
 }
 
-h1, h2 {
-  font-weight: normal;
+.container {
+	max-width: 100vw;
+	display: block;
+	text-align: center;
+	font-family: Helvetica, sans-serif;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.content {
+	display: flex;
+	flex-wrap: wrap;
+	margin: 0 auto;
+	justify-content: center;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.images-content {
+	width: 25%;
+	padding: 10px;
+	box-sizing: border-box;
+	margin-bottom: 10px;
+}
+.images-content h2 {
+	background: #3498db;
+	margin-bottom: 0;
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border: 5px solid #000;
 }
 
-a {
-  color: #42b983;
+.images {
+	width: 100%;
+	height: 280px;
+	-webkit-box-shadow: 10px 10px 29px -5px rgba(0,0,0,0.75);
+	-moz-box-shadow: 10px 10px 29px -5px rgba(0,0,0,0.75);
+	box-shadow: 10px 10px 29px -5px rgba(0,0,0,0.75);
 }
 </style>
